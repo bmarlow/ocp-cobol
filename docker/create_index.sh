7173f247ee720a0975c8579508959a40bd00ffc8
Brandon Marlow <brandon.n.marlow@gmail.com>
2020-05-26 16:32:17 -0700

    remove old folder

--- docker/create_index.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-/src/helloworld-exe >> /tmp/index.html
-echo "</body>" >> /tmp/index.html
-echo "COBOL Compiler info:<p>" >> /tmp/index.html
-cobcrun --version >> /tmp/index.html
-echo "</html>" >> /tmp/index.html
